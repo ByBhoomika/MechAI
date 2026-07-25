d9f0f9182d78b47807bac1dcc304ea4aeb2ba1bd
Bhoomika Singh <bhoomikasingh2604@gmail.com>
2026-07-25 12:56:06 +0530

    Updated 3 files: app.py, routes_home.cpython-314.pyc, sitemap.xml

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask
-
 from mechai.routes_calculator import calculator
 from mechai.routes_converter import converter
 from mechai.routes_materials import materials
 from mechai.routes_machine_design import machine_design
 from mechai.services.routes_home import home
-
+from flask import send_from_directory
 app = Flask(__name__)
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory("static", "sitemap.xml")
+
+
 
 app.register_blueprint(home)
 app.register_blueprint(calculator)
